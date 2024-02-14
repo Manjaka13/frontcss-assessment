8428102e4f103038eee458f1056f94cdcd7c60bd
manjaka13 <manjaka.rajaonson@gmail.com>
2024-02-14 14:41:09 +0300

    add foot text to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,5 +12,10 @@ export const Main = () => (
 		<Premium />
 		<PersonalForm />
 		<Delete />
+		<p className="main__info">
+			<span className="primary">Get in touch with our support team</span> if you
+			have any question or want to leave some feedback. We’ll be happy to hear from
+			you.
+		</p>
 	</div>
 );
