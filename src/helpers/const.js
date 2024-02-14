aa6a88c9b66abd1f5bb416b226604905a7e0b8dc
manjaka13 <manjaka.rajaonson@gmail.com>
2024-02-14 13:35:06 +0300

    add constants

---
+++ src/helpers/const.js
@@ -0,0 +1,31 @@
+/**
+ * Holds our constants
+ */
+
+export const NAV_TOP_SEARCH = [
+	"Software Engineer",
+	"Computer hardware engineer",
+	"Network Engineer",
+	"Technical Support",
+	"Network Admnistrator",
+	"Management",
+	"Data analysis",
+	"Computer technician",
+];
+
+export const NAV_LAST_SEARCH = [
+	"Past search 1",
+	"Past search 2",
+	"Computers and information",
+	"Database Admnistrator",
+	"Computer security",
+	"Computer Systems Analyst",
+];
+
+export const NAV_BOARD = ["Board 1", "Board 2", "Board 3"];
+
+export const NAV_BOARD_AGENT = [
+	"Board agent 1",
+	"Board agent 1",
+	"Board agent 1",
+];
