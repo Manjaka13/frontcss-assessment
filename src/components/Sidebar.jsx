2eb908f77cc7e6837748c7c3f6a054a89f15bd63
manjaka13 <manjaka.rajaonson@gmail.com>
2024-02-14 12:39:28 +0300

    add sidebar and title

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../assets/Resumizeme.svg";
+
+/**
+ * Page sidebar
+ */
+
+export const Sidebar = () => (
+	<div className="sidebar">
+		<div className="sidebar__title">
+			<img className="logo" src={Logo} alt="Logo" />
+		</div>
+	</div>
+);
